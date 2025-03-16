d98159f95778865071f0fee4a22ea288fc71e8cd
Eka Yulianto <thepilotrock07@gmail.com>
2025-03-16 16:08:27 +0700

    add:lib+get

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({ Key? key }) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.maxFinite,
+        width: double.maxFinite,
+        color: const Color.fromRGBO(17, 17, 16, 1.0),
+      ),
+    );
+  }
+}
